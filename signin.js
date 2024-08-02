de7a9c0c6dc798fa5cf8fcd6fa1438036a7c994c
Md Khayam Hawari <khayamraza92@gmail.com>
2024-08-02 15:20:22 +0530

    Updated

---
+++ signin.js
@@ -0,0 +1,48 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { getDatabase, ref, get, child } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAbRcpzO4qMrRdRGiGg6Z7Vh00pQzKJqDQ",
+  authDomain: "quicktrip-project.firebaseapp.com",
+  projectId: "quicktrip-project",
+  storageBucket: "quicktrip-project.appspot.com",
+  messagingSenderId: "82813329780",
+  appId: "1:82813329780:web:489f6f51bc03a72fbe8c21"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+const auth = getAuth(app);
+const dbref = ref(db);
+
+const UserEmail = document.getElementById('useremail');
+const UserPassword = document.getElementById('userpass');
+const SignInBtn = document.getElementById('signin-submit-btn');
+
+const SignInUser = () => {
+    // evt.preventDefault();
+
+    signInWithEmailAndPassword(auth, UserEmail.value, UserPassword.value)
+        .then((userCredential) => {
+            // Sign In 
+            console.log(userCredential);
+            alert('Login Successfully!')
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode);
+            console.log(errorMessage);
+        });
+        window.location.href = './packages.html';
+}
+
+SignInBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    SignInUser();
+});
