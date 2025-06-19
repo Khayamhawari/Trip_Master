b2dbb6483a7a75a66fba57804ccca795505d06cc
Md Khayam Hawari <khayamraza92@gmail.com>
2025-06-19 16:03:27 +0530

    New Pages added

---
+++ destinations/swiperjs.js
@@ -0,0 +1,17 @@
+ var swiper = new Swiper(".mySwiper", {
+            loop: true,
+            autoplay: {
+                delay: 4000,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            grabCursor: true,
+            spaceBetween: 30,
+        });
